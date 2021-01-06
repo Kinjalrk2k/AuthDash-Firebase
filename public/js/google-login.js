cde49daeb6a50b4be3964e9c781ade70f2b8eee1
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-01-06 18:07:57 +0530

    added login with google

---
+++ public/js/google-login.js
@@ -0,0 +1,8 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start("#firebaseui-auth-container", {
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  signInFlow: "popup",
+  signInSuccessUrl: "/dashboard",
+});
